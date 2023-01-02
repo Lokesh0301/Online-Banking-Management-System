1fda16ae25fb236302d66cf9d300db63247318b3
Lokesh0301 <68785961+Lokesh0301@users.noreply.github.com>
2023-01-02 14:27:42 -0600

    Add files via upload

---
+++ src/UIManager.java
@@ -0,0 +1,7 @@
+class UIManager {
+
+    static void setLookAndFeel(String comjtattooplafsmartSmartLookAndFeel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
